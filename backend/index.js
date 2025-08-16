447cda8152ec83ecb787f284b14d24daa670a155
kartik <raghu1ckartik.rk@gmail.com>
2025-08-17 01:28:53 +0530

    modified

--- backend/index.js
+++ backend/index.js
@@ -18,10 +18,7 @@ const userRouter = require("./routes/userRouter");
 const authRouter = require("./routes/authRouter");
 
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("MongoDB connected"))
 .catch(err => console.error("MongoDB connection error:", err));
 
